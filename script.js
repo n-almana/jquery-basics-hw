c77ad296acb3833bd41646568a28e24a55810c49
Noora Al-Mana <nalmana.babson@gmail.com>
2016-05-06 20:06:23 -0400

    Added some jQuery

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.log("Is this working?")
-$ ("#header").fadeOut();
+console.log("Is this working?");
+$('#one').slideUp(3000);
+$('#two').animate(3000);
+$('#three').show();
+
+$("#header").mouseenter(function(){
+    $(this).css("background-color", "lightblue");
+});
+
+$("#header").mouseleave(function(){
+    $(this).css("background-color", "white");
+});
